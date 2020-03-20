ed056e60d63f901692b91c745550cb8bbf204c88
Hook <hookhookun@gmail.com>
2020-03-20 15:28:06 +0900

    Add getPNGInfo

---
+++ src/util/getPNGInfo.test.ts
@@ -0,0 +1,27 @@
+import test from 'ava';
+import {getPNGInfo} from './getPNGInfo';
+import {Base64} from './base64';
+
+const png8x6 = Base64.decode([
+    'iVBORw0KGgoAAAANSUhEUgAAAAgAAAAGCAMAAADJ2y/JAAAABGdBTUEAALGPC/x',
+    'hBQAAAAFzUkdCAK7OHOkAAABIUExURQAAAJkvWcxyr4QuUbl2rbROgaxEdrVbmq',
+    '5PhrNWkXwlPKZEd5pDWqg+aFYxOrlamKA9a5YzYJ1DgpdRiZAmUGE9S7Zhhq1Wd',
+    '29cwz8AAAABdFJOUwBA5thmAAAANklEQVQI1xXGWwKAIAgEwFVBIFLLR93/puV8',
+    'DdhLwBZN/Tx2xJRzBbo0U13AfcnvTZhhENGTPiK+AYD2ttbOAAAAAElFTkSuQmCC'
+].join());
+
+test('Load an 8x6 png image', (t) => {
+    const result = getPNGInfo(png8x6);
+    t.deepEqual(
+        result,
+        {
+            width: 8,
+            height: 6,
+            bitDepth: 8,
+            colorType: 3,
+            compressionMethod: 0,
+            filterMethod: 0,
+            interlaceMethod: 0,
+        },
+    );
+});
--- src/util/parsePNG.test.ts
+++ src/util/parsePNG.test.ts
@@ -2,7 +2,13 @@ import test from 'ava';
 import {parsePNG} from './parsePNG';
 import {Base64} from './base64';
 
-const png8x6 = Base64.decode('iVBORw0KGgoAAAANSUhEUgAAAAgAAAAGCAMAAADJ2y/JAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAABIUExURQAAAJkvWcxyr4QuUbl2rbROgaxEdrVbmq5PhrNWkXwlPKZEd5pDWqg+aFYxOrlamKA9a5YzYJ1DgpdRiZAmUGE9S7Zhhq1Wd29cwz8AAAABdFJOUwBA5thmAAAANklEQVQI1xXGWwKAIAgEwFVBIFLLR93/puV8DdhLwBZN/Tx2xJRzBbo0U13AfcnvTZhhENGTPiK+AYD2ttbOAAAAAElFTkSuQmCC');
+const png8x6 = Base64.decode([
+    'iVBORw0KGgoAAAANSUhEUgAAAAgAAAAGCAMAAADJ2y/JAAAABGdBTUEAALGPC/x',
+    'hBQAAAAFzUkdCAK7OHOkAAABIUExURQAAAJkvWcxyr4QuUbl2rbROgaxEdrVbmq',
+    '5PhrNWkXwlPKZEd5pDWqg+aFYxOrlamKA9a5YzYJ1DgpdRiZAmUGE9S7Zhhq1Wd',
+    '29cwz8AAAABdFJOUwBA5thmAAAANklEQVQI1xXGWwKAIAgEwFVBIFLLR93/puV8',
+    'DdhLwBZN/Tx2xJRzBbo0U13AfcnvTZhhENGTPiK+AYD2ttbOAAAAAElFTkSuQmCC'
+].join());
 
 test('Load an 8x6 png image', (t) => {
     const result = [...parsePNG(png8x6)];
