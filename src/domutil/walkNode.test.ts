c91d8f0decaed4f60de2e498267754d6902e6681
Hook <hookhookun@gmail.com>
2020-03-20 17:42:56 +0900

    Add walkNode

---
+++ src/domutil/walkNode.test.ts
@@ -0,0 +1,53 @@
+import ava, {ExecutionContext} from 'ava';
+import {JSDOM, DOMWindow} from 'jsdom';
+import {walkNode} from './walkNode';
+
+const test = (
+    html: string,
+    callback: (t: ExecutionContext, window: DOMWindow) => void | Promise<void>,
+): void => {
+    ava(html, async (t) => {
+        const {window} = new JSDOM(`<!doctype html>${html}`, {
+            url: 'https://example.com',
+        });
+        Object.assign(global, {
+            Node: window.Node,
+        });
+        await callback(t, window);
+    });
+};
+
+test(
+    '<p>AAA</p>',
+    (t, {document}) => {
+        const results = [];
+        for (const step of walkNode(document.body)) {
+            results.push(step);
+        }
+        const {body} = document;
+        const p = body.querySelector('p') as Node;
+        const text: Text = p.firstChild as Text;
+        t.deepEqual(results, [
+            {
+                type: walkNode.Enter,
+                node: document.body,
+            },
+            {
+                type: walkNode.Enter,
+                node: p,
+            },
+            {
+                type: walkNode.Text,
+                node: text,
+            },
+            {
+                type: walkNode.Leave,
+                node: p,
+            },
+            {
+                type: walkNode.Leave,
+                node: document.body,
+            },
+        ]);
+    },
+);
