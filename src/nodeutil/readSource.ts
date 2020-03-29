1dffe7820d3694ff55185f8013a8022335b3c412
Hook <hookhookun@gmail.com>
2020-03-30 00:42:06 +0900

    Cache resources from URL

--- src/nodeutil/readSource.ts
+++ src/nodeutil/readSource.ts
@@ -2,11 +2,18 @@ import * as fs from 'fs';
 import {httpsGet} from './httpsGet';
 import {readStream} from './readStream';
 
+const cache = new Map<string, Buffer>();
+
 export const readSource = async (
     filePathOrURL: string,
 ): Promise<Buffer> => {
     if (filePathOrURL.startsWith('https://')) {
-        return await readStream(await httpsGet(filePathOrURL));
+        let cached = cache.get(filePathOrURL);
+        if (!cached) {
+            cached = await readStream(await httpsGet(filePathOrURL));
+            cache.set(filePathOrURL, cached);
+        }
+        return cached;
     } else {
         return fs.promises.readFile(filePathOrURL);
     }
